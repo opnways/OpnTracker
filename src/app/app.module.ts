018b6cd787849a7812bfdfd1c114eee54b669a54
opnways <admin@opnways.com>
2022-08-28 17:50:02 +0200

    Show Issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import {Issue } from './issue';
+import { issues } from '../assets/mock-issues';
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
-  private issues: Issue[] = [];
+  private issues: Issue[] = issues;
   constructor() { }
 
   getPendingIssues(): Issue[] {
