814909491ae8d1dedfaeab38f6159b39c2844869
opnways <admin@opnways.com>
2022-08-28 19:40:37 +0200

    Confirm Dialog

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -10,6 +10,7 @@ import { IssuesService } from '../issues.service';
 export class IssueListComponent implements OnInit {
   issues: Issue[] = [];
   showReportIssue = false;
+  selectedIssue: Issue | null = null;
   constructor( private issueService: IssuesService) { }
 
   ngOnInit(): void {
@@ -20,6 +21,17 @@ export class IssueListComponent implements OnInit {
   }
   onCloseReport() {
     this.showReportIssue = false;
+    this.getIssues();
+  }
+  onConfirm(confirmed: boolean) {
+    if (confirmed && this.selectedIssue) {
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
+  }
+  onModify(issue: Issue) {
+    this.issueService.modifyIssue(issue);
     this.getIssues();
   }
 
